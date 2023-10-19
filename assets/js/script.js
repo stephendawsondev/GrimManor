860a8fe558ca2407f98e52ad1f2520b81cefcea9
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-19 21:21:32 +0000

    Add function to display second mini game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@
 
 // This function displays the first mini game
 const miniGame1 = () => window.location.href = "game1.html";
+
+// This function displays the second mini game
+const miniGame2 = () => window.location.href = "game2.html";
