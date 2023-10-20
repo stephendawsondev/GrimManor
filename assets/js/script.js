e0b8a37e67bfcdc73ebe5b131f9ccec1cc9307f9
A. Urquiola <patchamama.go@gmail.com>
2023-10-20 18:57:12 +0200

    add interactive hidde buttons to the background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,44 @@
 const backgroundImage = document.getElementById("background-image");
 const moveButtons = document.querySelectorAll("button");
 const container = document.getElementById("container");
+const DEBUT = false;
+
+window.onload = function () {
+  //backgroundImage.style.bottom = "-100px";
+};
+
+// Debug actions of button (show/hide buttons)
+if (DEBUT) {
+  document.onmousemove = function (e) {
+    var x = e.pageX;
+    var y = e.pageY;
+    e.target.title = "X is " + x + " and Y is " + y;
+  };
+}
+const buttonDebug = document.getElementById("button-debug");
+buttonDebug.addEventListener("click", function () {
+  const buttons = document.querySelectorAll(".interactive");
+  buttons.forEach((button) => {
+    button.classList.toggle("show");
+  });
+});
+
+// Select all buttons with the "interactive" class
+const interactiveButtons = document.querySelectorAll(".interactive");
+// Add a click event to each button
+interactiveButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    alert(`Button ID: ${this.id}`);
+  });
+});
+
+function updateButtonsPos(top, left) {
+  const buttons = document.querySelectorAll(".interactive");
+  buttons.forEach((button) => {
+    button.style.top = button.offsetTop + top + "px";
+    button.style.left = button.offsetLeft + left + "px";
+  });
+}
 
 moveButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
@@ -14,8 +52,10 @@ function moveBackground(direction) {
   let left = backgroundImage.offsetLeft;
 
   const step = 20; // Adjust this to change the speed of the movement
-  const containerWidth = container.clientWidth;
-  const containerHeight = container.clientHeight;
+  //const containerWidth = container.clientWidth;
+  //const containerHeight = container.clientHeight;
+  const containerWidth = window.innerWidth;
+  const containerHeight = window.innerHeight;
   const imageWidth = backgroundImage.width;
   const imageHeight = backgroundImage.height;
 
@@ -46,6 +86,10 @@ function moveBackground(direction) {
     imageHeight,
     top - step,
     containerHeight - imageHeight
+  );
+  updateButtonsPos(
+    top - backgroundImage.offsetTop,
+    left - backgroundImage.offsetLeft
   );
   //backgroundImage.style.top = top + "px";
   //backgroundImage.style.left = left + "px";
