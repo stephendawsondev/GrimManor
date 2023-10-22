08f02ae808e8fb79e307f096f3fe72ff2bf54216
Stephen Dawson <stephendawson@protonmail.com>
2023-10-22 12:59:49 +0100

    add code to draw hangman

--- assets/js/minigames/hangman.js
+++ assets/js/minigames/hangman.js
@@ -2,17 +2,107 @@ import { showDialogueAsync } from "../dialogue.js";
 
 const guessedLetters = [];
 
-const checkLetter = (letter, buttonElement) => {
+const drawInitialScene = () => {
+  const canvas = document.getElementById("stickman");
+  const context = canvas.getContext("2d");
+  context.lineWidth = 4;
+
+  context.beginPath();
+  context.moveTo(20, 180);
+  context.lineTo(180, 180);
+  context.moveTo(100, 180);
+  context.lineTo(100, 40);
+  context.lineTo(160, 40);
+  context.lineTo(160, 80);
+  context.stroke();
+
+  context.beginPath();
+  context.moveTo(160, 80);
+  context.lineTo(160, 80);
+  context.stroke();
+};
+
+let incorrectGuessCount = 0;
+const drawHangman = () => {
+  const canvas = document.getElementById("stickman");
+  const context = canvas.getContext("2d");
+  context.lineWidth = 4;
+
+  switch (incorrectGuessCount) {
+    case 0:
+      // Draw hanging apparatus
+      context.beginPath();
+      context.moveTo(20, 180);
+      context.lineTo(180, 180);
+      context.moveTo(100, 180);
+      context.lineTo(100, 40);
+      context.lineTo(160, 40);
+      context.lineTo(160, 80);
+      context.stroke();
+      break;
+    case 1:
+      // Draw head
+      context.beginPath();
+      context.arc(160, 90, 10, 0, Math.PI * 2);
+      context.stroke();
+      break;
+    case 2:
+      // Draw body
+      context.beginPath();
+      context.moveTo(160, 100);
+      context.lineTo(160, 140);
+      context.stroke();
+      break;
+    case 3:
+      // left arm
+      context.beginPath();
+      context.moveTo(160, 110);
+      context.lineTo(145, 125);
+      context.stroke();
+      break;
+    case 4:
+      // right arm
+      context.beginPath();
+      context.moveTo(160, 110);
+      context.lineTo(175, 125);
+      context.stroke();
+      break;
+    case 5:
+      // left leg
+      context.beginPath();
+      context.moveTo(160, 140);
+      context.lineTo(145, 155);
+      context.stroke();
+      break;
+    case 6:
+      // right leg
+      context.beginPath();
+      context.moveTo(160, 140);
+      context.lineTo(175, 155);
+      context.stroke();
+      break;
+  }
+};
+
+const checkLetter = (letter, buttonElement, phraseArr) => {
   if (guessedLetters.includes(letter)) return;
 
   guessedLetters.push(letter);
-
+  if (
+    !phraseArr
+      .map((letter) => letter.toUpperCase())
+      .includes(letter.toUpperCase())
+  ) {
+    incorrectGuessCount++;
+    drawHangman();
+  }
   // Disable the button and change its appearance
   buttonElement.disabled = true;
   buttonElement.classList.add("disabled");
   buttonElement.setAttribute("aria-disabled", "true");
 
   const letterSpans = document.querySelectorAll(".hangman-letter");
+
   for (const span of letterSpans) {
     if (
       guessedLetters
@@ -30,6 +120,8 @@ const runHangmanGame = () => {
   const hangmanContainer = document.getElementById("hangman-game");
 
   hangmanContainer.classList.add("active");
+
+  drawInitialScene();
 
   const phraseArr = "She married someone else".split("");
 
@@ -68,7 +160,7 @@ const runHangmanGame = () => {
     }
 
     button.addEventListener("click", () => {
-      checkLetter(button.innerText, button);
+      checkLetter(button.innerText, button, phraseArr);
     });
   }
 
